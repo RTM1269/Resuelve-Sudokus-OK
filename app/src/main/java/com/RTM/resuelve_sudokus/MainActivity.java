f7c10fc13721a8a4c0b6909a1b24c08be9ef0215
RTM1269 <raul.torrecilla.moreno@alumnojoyfe.iepgroup.es>
2021-10-25 15:18:40 +0200

    v.3

--- app/src/main/java/com/RTM/resuelve_sudokus/MainActivity.java
+++ app/src/main/java/com/RTM/resuelve_sudokus/MainActivity.java
@@ -1,20 +1,51 @@
 package com.RTM.resuelve_sudokus;
 
+import static java.lang.System.out;
+import static java.sql.DriverManager.println;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.Debug;
 
 import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.Console;
 
 public class MainActivity extends AppCompatActivity {
-private EditText etn;
+private EditText etn ;
+private String[] lista_et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etn = findViewById(R.id.et0000);
 
+        int i = 0;
+        lista_et = new String[81];
+        String code;
+            for (int r = 0; r < 3 ; r++){
+                for (int c = 0; c < 3; c++){
+                    for (int f = 0; f < 3; f++){
+                        for (int p = 0; p < 3; p++){
+                            code = "et" + String.valueOf(r) + String.valueOf(c) + String.valueOf(f) + String.valueOf(p);
+                            int etID = getResources().getIdentifier(code,"id",getPackageName());
+                            etn = findViewById(etID);
+                            Log.i("ID",code);
+                            lista_et[i]=code;
+                            i++;
+                        }
+                    }
+                }
+            }
+    }
+    public void imprimirListaEditText(){
+        for (int i=0; i<lista_et.length;i++){
+            Log.d("idS",lista_et[i]);
+        }
     }
 
 
